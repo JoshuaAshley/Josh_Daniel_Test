736f2dacf82ef7a824ef8c93499c39a3eb63c5f5
JoshuaAshley <st10060590@vcconnect.edu.za>
2024-02-20 09:13:09 +0200

    (Josh) 00-Initial Commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Josh_Daniel_Test_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
